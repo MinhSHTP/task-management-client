319d4aac8204531b895c39df6b3d3674162a63e1
MinhHuynh <minhhuynh17293@gmail.com>
2023-09-26 22:45:06 +0700

    Implement app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import { Login, Dashboard } from "@pages";
+import { Routes, Route } from "react-router-dom";
+import { APP_PATH } from "@utils";
 
 function App() {
   return (
     <>
-      <Login />
-      <Dashboard />
+      <Routes>
+        <Route path={APP_PATH.LOGIN_ROUTE} element={<Login />} />
+        <Route path={APP_PATH.DASHBOARD_ROUTE} element={<Dashboard />} />
+        <Route path="/*" element={<>404-Not found</>} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/utils/constants.ts
@@ -0,0 +1,8 @@
+const loginRoute = "/login";
+const dashboardRoute = "/dashboard";
+const APP_PATH = {
+  LOGIN_ROUTE: loginRoute,
+  DASHBOARD_ROUTE: dashboardRoute,
+};
+
+export { APP_PATH };
