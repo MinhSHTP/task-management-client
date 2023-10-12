9458e6cb2203144138d1cca6aac60c06bdc09ee1
MinhHuynh <minhhuynh17293@gmail.com>
2023-10-13 01:43:31 +0700

    Raw login page

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,3 +1,51 @@
 export const Login = () => {
-  return <>Login</>;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          background: "red",
+          flexDirection: "column",
+          justifyItems: "center",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ flex: 1, background: "blue", width: "100%" }}>
+          <p>Top LOGIN PAGE</p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          background: "red",
+          flexDirection: "column",
+          justifyItems: "center",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ flex: 1, background: "red", width: "100%" }}>
+          <p>LOGIN PAGE</p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          background: "red",
+          flexDirection: "column",
+          justifyItems: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            background: "green",
+            width: "100%",
+          }}
+        >
+          <p>Footer LOGIN PAGE</p>
+        </div>
+      </div>
+    </>
+  );
 };
